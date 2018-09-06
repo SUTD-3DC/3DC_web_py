39e31e5aa1f9980d01d00ea99e96680302563b39
TAY TS <taytzushieh@gmail.com>
2018-09-06 18:17:06 +0800

    feature: Add NotFound page

--- app/frontend/src/router/index.js
+++ app/frontend/src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import NotFound from '@/components/NotFound'
+
+// This is to auto import all the components
+// and map the endpoint with the component
+// const routerOptions = [
+//   { path: '/', component: 'Home' },
+//   { path: '/about', component: 'About' }
+// ]
+// const routes = routerOptions.map(route => {
+//   return {
+//     ...route,
+//     component: () => import(`@/components/${route.component}.vue`)
+//   }
+// })
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '*',
+      name: 'ErrorPage',
+      component: NotFound
     }
   ]
 })
